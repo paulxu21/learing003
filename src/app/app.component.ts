253cf7290a8404631a885bc578c71afdc082a67a
paulxu21 <paul_xu21@hotmail.com>
2019-03-02 16:40:14 +0800

    add shareModulenew

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(private breakpointObserver: BreakpointObserver) {}
   title = 'learing003';
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches)
+    );
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DemoNavComponent } from './demo-nav/demo-nav.component';
 import { ShareModule } from './share/share.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DemoNavComponent
   ],
   imports: [
     BrowserModule,
